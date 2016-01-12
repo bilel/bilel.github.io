b628cfe72f046c1fbb70658e59ae66a47e451f7e
Bilel Hawari <bilelhawari@gmail.com>
2016-01-12 03:51:51 +0100

    Update weather.js

--- scripts/weather.js
+++ scripts/weather.js
@@ -21,15 +21,44 @@ clearTimeout(x_timer);
 		$('.temp').html(temp);
 		$('.wind').html(' Direction : '+winddir+' Speed: '+speed);
 		
-		console.log(data);
-		console.log(winddir);
+	 });
 
-      
-    });
 
 }
 }, 300);
 });
+
+    var urlip = 'https://api.ipify.org/?format=json';
+    $.get(urlip, function(ipdata){
+    	var ip = ipdata.ip;
+    });
+    if(ip){
+    var urlgeo = 'http://freegeoip.net/json/'+ip;
+    $.get(urlgeo, function(geodata){
+    	var geo = geodata.city;
+    	var code = geodata.country_code;
+    	if(geo!=''){
+    		$('.yourcity').html(geo);
+    		$('.clic').html('<==== Just Click if this is your City!');
+    		var newurl = 'http://api.openweathermap.org/data/2.5/weather?q='+geo+','+code+'&units=metric&appid=2de143494c0b295cca9337e1e96b00e0';
+    		$('a').on("click",function(e){
+    			
+    		$.get(url, function(data){
+		  var temp = data.main.temp;
+		  var winddir = degToCompass(data.wind.deg);
+		  var speed = data.wind.speed;
+		  var icon = data.weather[0].icon;
+		  
+		$('.icon').attr("src","http://openweathermap.org/img/w/"+icon+".png");  
+		$('.temp').html(temp);
+		$('.wind').html(' Direction : '+winddir+' Speed: '+speed);
+		
+	 });
+    			
+    		});
+    	}
+    });
+    }
 
 window.onload = function() {
   document.getElementById("q").focus();
